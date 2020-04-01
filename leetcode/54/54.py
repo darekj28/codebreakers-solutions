093427dd04e233e3cba77a36969ecdd97a101096
cbawc <ngoy.deliberate.learning@gmail.com>
2020-04-01 09:08:12 -0700

    changed variable names

--- leetcode/54/54.py
+++ leetcode/54/54.py
@@ -6,37 +6,37 @@ class Solution(object):
         if len(matrix) == 0:
             return sol
         # initialize bounds for perimeter to traverse
-        minR = 0
-        minC = 0
-        maxR = len(matrix)-1
-        maxC = len(matrix[0])-1
+        minRow = 0
+        minCol = 0
+        maxRow = len(matrix)-1
+        maxCol = len(matrix[0])-1
         
         # loop through perimeter and record values
-        while minR <= maxR and minC <= maxC:
+        while minRow <= maxRow and minCol <= maxCol:
             # record alone the top edge
-            for c in range(minC,maxC+1):
-                sol.append(matrix[minR][c])
+            for c in range(minCol,maxCol+1):
+                sol.append(matrix[minRow][c])
         
             # record along the right edge
-            for r in range(minR+1,maxR+1):
-                sol.append(matrix[r][maxC])
+            for r in range(minRow+1,maxRow+1):
+                sol.append(matrix[r][maxCol])
             
             # record along the bottom edge
             # we don't want to record it if there's only one row in this perimeter
-            if minR < maxR:
-                for c in reversed(range(minC,maxC)):
-                    sol.append(matrix[maxR][c])
+            if minRow < maxRow:
+                for c in reversed(range(minCol,maxCol)):
+                    sol.append(matrix[maxRow][c])
                 
             # record along the left edge
             # we don't want to record it if there's only one column in this perimeter
-            if minC < maxC:
-                for r in reversed(range(minR+1,maxR)):
-                    sol.append(matrix[r][minC])
+            if minCol < maxCol:
+                for r in reversed(range(minRow+1,maxRow)):
+                    sol.append(matrix[r][minCol])
             
             # update perimeter
-            minR += 1
-            maxC -= 1
-            maxR -= 1
-            minC += 1
+            minRow += 1
+            maxCol -= 1
+            maxRow -= 1
+            minCol += 1
             
         return sol
